09b91f3a15096a86a67b267490b5387b6ba9e76c
Akhmad Fauzy Epriliawan <ak.fauzy81@gmail.com>
2019-05-01 09:26:42 +0700

    kl

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page | Peminjaman Kelas</title>
+    <link rel="stylesheet" href="Assets/Login.css">
+  </head>
+  <body>
+    <div class="background">
+      <div class="container">
+        <div class="row">
+          <div class="form" style="height:100%;padding:10% 0px;">
+            <h2>LOGIN FORM</h2>
+            <form action="" method="post">
+              <input type="text" name="username" placeholder="Username" />
+              <input type="password" name="password" placeholder="Password" />
+              <input type="submit" name="login" value="LOGIN" />
+            </form>
+            <hr>
+            <p>
+              Please Login with your SSO account!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
